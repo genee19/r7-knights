8188a05e9e70ff8d794dea8ab95d5ca7368e209b
Eugene Efimochkin <e.efimochkin@gmail.com>
2022-06-12 16:43:36 +0200

    Actually play the game and display its results

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,6 +4,11 @@ class GamesController < ApplicationController
   end
 
   def create
-    
+    @game = Game.new(
+      params[:character_number].to_i,
+      turn_direction: params[:turn_direction].to_i,
+      attack_direction: params[:attack_direction].to_i
+    )
+    @game.play!
   end
 end
