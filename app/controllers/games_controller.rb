74cc45a415f2dad1e02e209571b8432a18cf246f
Eugene Efimochkin <e.efimochkin@gmail.com>
2022-06-12 23:05:58 +0200

    Add options to generate witches to the form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,7 +7,8 @@ class GamesController < ApplicationController
     @game = Game.new(
       params[:character_number].to_i,
       turn_direction: params[:turn_direction].to_i,
-      attack_direction: params[:attack_direction].to_i
+      attack_direction: params[:attack_direction].to_i,
+      witches_percentage: params[:witches_percentage].to_i,
     )
     @game.play!
     @page_title = "Game of #{@game.character_number} Knights"
